e269bb6ef6853a145305544f67442d2acdd28181
jakob <jakob.germano@gmail.com>
2021-12-27 18:39:44 -0700

    Codealong

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,8 +8,13 @@ class MoviesController < ApplicationController
 
   def show
     movie = Movie.find(params[:id])
-    render json: movie
+  render json: movie
+    
   end
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end 
 
   private
 
